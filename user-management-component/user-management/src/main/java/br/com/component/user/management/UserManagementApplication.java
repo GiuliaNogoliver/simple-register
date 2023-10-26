031336b00229b8c4fc61f6ad544aa97622b83a82
Giulianogoliver <giulianogoliver84@gmail.com>
2023-10-26 20:37:24 -0300

    controller and service

--- user-management-component/user-management/src/main/java/br/com/component/user/management/UserManagementApplication.java
+++ user-management-component/user-management/src/main/java/br/com/component/user/management/UserManagementApplication.java
@@ -2,8 +2,10 @@ package br.com.component.user.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories({"br.com.user.management.repository"})
 public class UserManagementApplication {
 
 	public static void main(String[] args) {
--- user-management-component/user-management/src/main/java/br/com/user/management/service/CompanyService.java
+++ user-management-component/user-management/src/main/java/br/com/user/management/service/CompanyService.java
@@ -1,7 +1,10 @@
 package br.com.user.management.service;
 
+import org.springframework.stereotype.Service;
+
 import br.com.component.user.management.model.Company;
 
+@Service
 public interface CompanyService {
 
 	Company subscribeCampany(Company company);
